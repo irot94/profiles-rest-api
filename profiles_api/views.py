242ec5b1a1d884baa41af6e18632ffd780c5a406
Irek <irtarasek@gmail.com>
2021-03-23 08:41:34 +0100

    Added POST, PUT, PATCH and DELETE to APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 # Create your views here.
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
 
     def get(self, request, format=None):
@@ -18,3 +21,33 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+
+            )
+
+    def put(self, request, pk=None): #pk == Primary Key, PUT replaces an object with object that was provided
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None): # Updates only fields in objects that were provided in request
+        """Handle a partial update of an object"""
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
+
+        
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2(i_vk^eifalofp*l&rg1^m5eb!=n^-=s$$&kwfwn)ocn(cb1w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.171', '127.0.0.1']
 
 
 # Application definition
